4dfd20d6fcdd1c1f2089773561ab3e3005afe760
m-oswal <moksh.oswalvkc@gmail.com>
2024-11-16 19:35:20 +0530

    update links

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -9,7 +9,7 @@
        <h1 className='sectionsTitle'>Projects</h1>
        <div className={styles.projectsContainer}>
        <ProjectCard src={dummy} url="https://github.com/m-oswal/Sprout" projectname="Sprout" projectdesc="E-learning Platform" />
-        <ProjectCard src={daanbox} url="https://github.com/m-oswal/Sprout" projectname="DaanBox" projectdesc="Medical Equipment Donation App" />
+        <ProjectCard src={daanbox} url="https://github.com/pranav-suri/daanbox" projectname="DaanBox" projectdesc="Medical Equipment Donation App" />
         
        </div>
      </section>
